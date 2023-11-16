e0b531cc3d196df9a2fec46d0466392547b31ab5
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-16 11:48:52 +0600

    add delete workout

--- frontend/src/components/WorkoutCard.jsx
+++ frontend/src/components/WorkoutCard.jsx
@@ -1,5 +1,21 @@
 /* eslint-disable react/prop-types */
+import axios from "axios";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutCard = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:5000/api/workouts/${id}`)
+      .then((res) => {
+        if (res.status === 200) {
+          dispatch({ type: "DELETE_WORKOUT", payload: res.data });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="workout-card">
       <h2>{workout.title}</h2>
@@ -12,6 +28,7 @@ const WorkoutCard = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt.toString()}</p>
+      <span onClick={() => handleDelete(workout._id)}>delete</span>
     </div>
   );
 };
