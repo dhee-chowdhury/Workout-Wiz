0b838e40e469b4f6f56e9c1b5d2b655ad60bbd35
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-16 14:07:52 +0600

    add hash pass

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -14,4 +15,17 @@ const userSchema = new Schema({
   },
 });
 
+// static signup method
+userSchema.statics.signup = async (email, password) => {
+  const emailExists = await this.findOne({ email });
+
+  if (emailExists) {
+    throw Error("Email already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 module.exports = mongoose.model("users", userSchema);
