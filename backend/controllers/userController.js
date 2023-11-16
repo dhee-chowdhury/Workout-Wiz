61b397c29f83d5842c3d622b6b23ae2751b85587
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-16 14:26:03 +0600

    add signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,10 +11,13 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
+  const { email, password } = req.body;
   try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
     res.json({ message: "signup user" });
   } catch (error) {
-    res.json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
